d5ed6d363d0a7be13236226a9a75c98e2d42c259
Satish Jaiswal <ictish@gmail.com>
2015-10-03 02:33:36 -0400

    binbo

--- app/src/main/java/com/hacku/kuse/hacku2015/Graph.java
+++ app/src/main/java/com/hacku/kuse/hacku2015/Graph.java
@@ -1,16 +1,125 @@
 package com.hacku.kuse.hacku2015;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import org.achartengine.ChartFactory;
+import org.achartengine.chart.BarChart;
+import org.achartengine.model.XYMultipleSeriesDataset;
+import org.achartengine.model.XYSeries;
+import org.achartengine.renderer.XYMultipleSeriesRenderer;
+import org.achartengine.renderer.XYSeriesRenderer;
 
 /**
  * Created by denjo on 15/10/02.
  */
-public class Graph extends Activity {
+public class Graph extends AppCompatActivity {
+    private View mChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.graph);
+        getSupportActionBar().hide();
+
+
+        openChart();
+
+        ImageButton btnHome = (ImageButton) findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Graph.this, MainActivity.class));
+            }
+        });
+
+        ImageButton btnGraph = (ImageButton) findViewById(R.id.btnGraph);
+        btnGraph.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Graph.this, Graph.class));
+            }
+        });
+
+    }
+
+    private void openChart(){
+
+        //Creating the data
+        int[] time = { 0,1,2,3,4,5,6,7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19 };
+        int[] intensity = { 112, 210, 150, 110, 50, 20, 30, 70, 80, 176,
+                245, 255, 67, 87, 187, 123, 176, 214, 245, 210 };
+
+        //Creating an XYSeries
+        XYSeries intensitySeries = new XYSeries("");
+        // Adding data to the Series
+        for(int i=0;i<time.length;i++){
+            intensitySeries.add(time[i],intensity[i]);
+        }
+
+        // Creating a dataset to hold the series
+        XYMultipleSeriesDataset dataset = new XYMultipleSeriesDataset();
+        // Adding Income Series to the dataset
+        dataset.addSeries(intensitySeries);
+
+        // Creating XYSeriesRenderer to customize incomeSeries
+        XYSeriesRenderer intensityRenderer = new XYSeriesRenderer();
+        intensityRenderer.setColor(Color.RED); //color of the graph set to cyan
+        intensityRenderer.setFillPoints(true);
+        intensityRenderer.setLineWidth(2);
+        intensityRenderer.setDisplayChartValues(true);
+        intensityRenderer.setDisplayChartValuesDistance(10); //setting chart value distance
+
+        // Creating a XYMultipleSeriesRenderer to customize the whole chart
+        XYMultipleSeriesRenderer multiRenderer = new XYMultipleSeriesRenderer();
+        multiRenderer.setOrientation(XYMultipleSeriesRenderer.Orientation.VERTICAL);
+        //multiRenderer.setXLabels(0);
+        //multiRenderer.setYLabels(0);
+        //multiRenderer.setChartTitle("Intensity of shaking legs");
+        //multiRenderer.setXTitle("Time[ago]");
+        //multiRenderer.setYTitle("Intensity");
+        multiRenderer.addSeriesRenderer(intensityRenderer);
+        multiRenderer.setBarSpacing(0.5);
+        multiRenderer.setMargins(new int[]{0, 0, 0, 0});
+        multiRenderer.setApplyBackgroundColor(true);
+        multiRenderer.setMarginsColor(Color.argb(0x00, 0x01, 0x01, 0x01));
+        multiRenderer.setBackgroundColor(Color.argb(0x00, 0x01, 0x01, 0x01));
+        multiRenderer.setXLabelsColor(Color.BLACK);
+        multiRenderer.setYLabelsColor(0,Color.BLACK);
+
+        //setting text size of the axis title
+        multiRenderer.setAxisTitleTextSize(30);
+        multiRenderer.setShowLegend(false);
+        multiRenderer.setShowGrid(true);
+        multiRenderer.setShowAxes(true);
+        multiRenderer.setInScroll(false);
+        multiRenderer.setAxesColor(Color.BLACK);
+
+        //setting text size of the graph lable
+        multiRenderer.setLabelsTextSize(24);
+
+        //this part is used to display graph on the xml
+        LinearLayout chartContainer = (LinearLayout) findViewById(R.id.chart);
+        //remove any views before u paint the chart
+        chartContainer.removeAllViews();
+        //drawing bar chart
+        mChart = ChartFactory.getBarChartView(Graph.this, dataset, multiRenderer, BarChart.Type.DEFAULT);
+        //adding the view to the linearlayout
+        chartContainer.addView(mChart);
     }
 }
--- app/src/main/java/com/hacku/kuse/hacku2015/MainActivity.java
+++ app/src/main/java/com/hacku/kuse/hacku2015/MainActivity.java
@@ -1,6 +1,11 @@
 package com.hacku.kuse.hacku2015;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,8 +14,18 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.sensormanager.SensorManager;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    private BluetoothAdapter bluetoothAdapter;
+    private SensorManager nekozeManager;
+    private SensorManager binboManager;
+    private final int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +34,12 @@ public class MainActivity extends AppCompatActivity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         //Remove notification bar
-//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
 
         ImageButton btnBinbo = (ImageButton) findViewById(R.id.btnBinbo);
         btnBinbo.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +64,104 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, Graph.class));
             }
         });
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if(bluetoothAdapter == null){
+            Toast.makeText(this, "Bluetooth is not supported!", Toast.LENGTH_SHORT);
+            finish();
+            return;
+        }
+
+        if(!bluetoothAdapter.isEnabled()){
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivity(enableIntent);
+        }else{
+            connectSensor();
+        }
+
+        int binboPoint = 0;
+        Drawable binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                getResources().getDrawable(R.drawable.icon_binbo, null) : getResources().getDrawable(R.drawable.icon_binbo);
+       while(true){
+         /* // if(binboManager.isNew()) {
+            //    binboPoint = (binboManager.getCurrentVal()) / 170 * 9; // 0 ~ 255
+            //}
+
+            switch(binboPoint) {
+                case 0:
+                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                            getResources().getDrawable(R.drawable.icon_binbo, null) : getResources().getDrawable(R.drawable.icon_binbo);
+                    break;
+                case 1:
+                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                            getResources().getDrawable(R.drawable.icon_binbo1, null) : getResources().getDrawable(R.drawable.icon_binbo1);
+                    break;
+                case 2:
+                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                            getResources().getDrawable(R.drawable.icon_binbo2, null) : getResources().getDrawable(R.drawable.icon_binbo2);
+                    break;
+                case 3:
+                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                            getResources().getDrawable(R.drawable.icon_binbo3, null) : getResources().getDrawable(R.drawable.icon_binbo3);
+                    break;
+                case 4:
+                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                            getResources().getDrawable(R.drawable.icon_binbo4, null) : getResources().getDrawable(R.drawable.icon_binbo4);
+                    break;
+                case 5:
+                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                            getResources().getDrawable(R.drawable.icon_binbo5, null) : getResources().getDrawable(R.drawable.icon_binbo5);
+                    break;
+                case 6:
+                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                            getResources().getDrawable(R.drawable.icon_binbo6, null) : getResources().getDrawable(R.drawable.icon_binbo6);
+                    break;
+                case 7:
+                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                            getResources().getDrawable(R.drawable.icon_binbo7, null) : getResources().getDrawable(R.drawable.icon_binbo7);
+                    break;
+                case 8:
+                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                            getResources().getDrawable(R.drawable.icon_binbo8, null) : getResources().getDrawable(R.drawable.icon_binbo8);
+                    break;
+                case 9:
+                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
+                            getResources().getDrawable(R.drawable.icon_binbo9, null) : getResources().getDrawable(R.drawable.icon_binbo9);
+                    break;
+            }*/
+            btnBinbo.setBackground(binboImage);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch (requestCode){
+            case REQUEST_ENABLE_BT :
+                if(resultCode == Activity.RESULT_OK){
+                    connectSensor();
+                }else{
+                    Toast.makeText(this, "can't enable Bluetooth", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    private void connectSensor(){
+        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+        if(pairedDevices.size() > 1) {
+            for (BluetoothDevice device : pairedDevices) {
+                if(device.getName().equals("KUSE-0")){
+                    binboManager = new SensorManager(device);
+                    binboManager.start();
+                }else if(device.getName().equals("KUSE-1")){
+                    nekozeManager = new SensorManager(device);
+                    nekozeManager.start();
+                }
+            }
+        }
     }
 
     @Override
