b60e421190bf4c4a05daae0937b29bcf4c6ba223
Satish Jaiswal <ictish@gmail.com>
2015-10-03 06:54:56 -0400

    working app

--- app/src/main/java/com/hacku/kuse/hacku2015/MainActivity.java
+++ app/src/main/java/com/hacku/kuse/hacku2015/MainActivity.java
@@ -6,8 +6,11 @@ import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.media.Image;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,12 +24,13 @@ import com.example.sensormanager.SensorManager;
 
 import java.util.Set;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Runnable {
     private BluetoothAdapter bluetoothAdapter;
     private SensorManager nekozeManager;
     private SensorManager binboManager;
     private final int REQUEST_ENABLE_BT = 1;
 
+    private Thread thread;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -41,8 +45,8 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().hide();
 
-        ImageButton btnBinbo = (ImageButton) findViewById(R.id.btnBinbo);
-        btnBinbo.setOnClickListener(new View.OnClickListener() {
+        ImageButton btnNekoze = (ImageButton) findViewById(R.id.btnNekoze);
+        btnNekoze.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(MainActivity.this, MainPopUp.class));
@@ -80,58 +84,8 @@ public class MainActivity extends AppCompatActivity {
             connectSensor();
         }
 
-        int binboPoint = 0;
-        Drawable binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                getResources().getDrawable(R.drawable.icon_binbo, null) : getResources().getDrawable(R.drawable.icon_binbo);
-       while(true){
-         /* // if(binboManager.isNew()) {
-            //    binboPoint = (binboManager.getCurrentVal()) / 170 * 9; // 0 ~ 255
-            //}
-
-            switch(binboPoint) {
-                case 0:
-                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                            getResources().getDrawable(R.drawable.icon_binbo, null) : getResources().getDrawable(R.drawable.icon_binbo);
-                    break;
-                case 1:
-                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                            getResources().getDrawable(R.drawable.icon_binbo1, null) : getResources().getDrawable(R.drawable.icon_binbo1);
-                    break;
-                case 2:
-                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                            getResources().getDrawable(R.drawable.icon_binbo2, null) : getResources().getDrawable(R.drawable.icon_binbo2);
-                    break;
-                case 3:
-                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                            getResources().getDrawable(R.drawable.icon_binbo3, null) : getResources().getDrawable(R.drawable.icon_binbo3);
-                    break;
-                case 4:
-                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                            getResources().getDrawable(R.drawable.icon_binbo4, null) : getResources().getDrawable(R.drawable.icon_binbo4);
-                    break;
-                case 5:
-                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                            getResources().getDrawable(R.drawable.icon_binbo5, null) : getResources().getDrawable(R.drawable.icon_binbo5);
-                    break;
-                case 6:
-                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                            getResources().getDrawable(R.drawable.icon_binbo6, null) : getResources().getDrawable(R.drawable.icon_binbo6);
-                    break;
-                case 7:
-                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                            getResources().getDrawable(R.drawable.icon_binbo7, null) : getResources().getDrawable(R.drawable.icon_binbo7);
-                    break;
-                case 8:
-                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                            getResources().getDrawable(R.drawable.icon_binbo8, null) : getResources().getDrawable(R.drawable.icon_binbo8);
-                    break;
-                case 9:
-                    binboImage = (android.os.Build.VERSION.SDK_INT > 20) ?
-                            getResources().getDrawable(R.drawable.icon_binbo9, null) : getResources().getDrawable(R.drawable.icon_binbo9);
-                    break;
-            }*/
-            btnBinbo.setBackground(binboImage);
-        }
+        thread = new Thread(this);
+        thread.start();
     }
 
     @Override
@@ -164,6 +118,129 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @Override
+    public void run(){
+        int binboPoint = 0;
+        int nekozePoint = 0;
+        while(true){
+            if(binboManager.isNew()) {
+                binboPoint = (binboManager.getCurrentVal()) * 9 / 50; // 0 ~ 255
+                if(binboPoint > 9) binboPoint = 9;
+                Log.d("LOG", "binboPoint : " + String.valueOf(binboPoint));
+                binboHandler.sendMessage(Message.obtain(binboHandler, binboPoint));
+                if(binboPoint > 3)
+                    characterHandler.sendMessage(Message.obtain(characterHandler, 1));
+                else
+                    characterHandler.sendMessage(Message.obtain(characterHandler, 0));
+            }
+
+            if(nekozeManager.isNew()) {
+                nekozePoint = (nekozeManager.getCurrentVal()) * 9 / 80;
+                if(nekozePoint > 9) nekozePoint = 9;
+                Log.d("LOG", "nekozePoint : " + String.valueOf(nekozePoint));
+                nekozeHandler.sendMessage(Message.obtain(nekozeHandler, nekozePoint));
+            }
+        }
+    }
+
+    private Handler binboHandler = new Handler(){
+        public void handleMessage(Message msg){
+            ImageButton btnBinbo = (ImageButton) findViewById(R.id.btnBinbo);
+            switch(msg.what){
+                case 0:
+                    btnBinbo.setImageResource(R.drawable.icon_binbo0);
+                    break;
+                case 1:
+                    btnBinbo.setImageResource(R.drawable.icon_binbo1);
+                    break;
+                case 2:
+                    btnBinbo.setImageResource(R.drawable.icon_binbo2);
+                    break;
+                case 3:
+                    btnBinbo.setImageResource(R.drawable.icon_binbo3);
+                    break;
+                case 4:
+                    btnBinbo.setImageResource(R.drawable.icon_binbo4);
+                    break;
+                case 5:
+                    btnBinbo.setImageResource(R.drawable.icon_binbo5);
+                    break;
+                case 6:
+                    btnBinbo.setImageResource(R.drawable.icon_binbo6);
+                    break;
+                case 7:
+                    btnBinbo.setImageResource(R.drawable.icon_binbo7);
+                    break;
+                case 8:
+                    btnBinbo.setImageResource(R.drawable.icon_binbo8);
+                    break;
+                case 9:
+                    btnBinbo.setImageResource(R.drawable.icon_binbo9);
+                    break;
+            }
+        }
+    };
+
+    private Handler nekozeHandler = new Handler(){
+        public void handleMessage(Message msg){
+            ImageButton btnNekoze = (ImageButton) findViewById(R.id.btnNekoze);
+            switch(msg.what){
+                case 0:
+                    btnNekoze.setImageResource(R.drawable.icon_nekoze0);
+                    break;
+                case 1:
+                    btnNekoze.setImageResource(R.drawable.icon_nekoze1);
+                    break;
+                case 2:
+                    btnNekoze.setImageResource(R.drawable.icon_nekoze2);
+                    break;
+                case 3:
+                    btnNekoze.setImageResource(R.drawable.icon_nekoze3);
+                    break;
+                case 4:
+                    btnNekoze.setImageResource(R.drawable.icon_nekoze4);
+                    break;
+                case 5:
+                    btnNekoze.setImageResource(R.drawable.icon_nekoze5);
+                    break;
+                case 6:
+                    btnNekoze.setImageResource(R.drawable.icon_nekoze6);
+                    break;
+                case 7:
+                    btnNekoze.setImageResource(R.drawable.icon_nekoze7);
+                    break;
+                case 8:
+                    btnNekoze.setImageResource(R.drawable.icon_nekoze8);
+                    break;
+                case 9:
+                    btnNekoze.setImageResource(R.drawable.icon_nekoze9);
+                    break;
+            }
+        }
+    };
+
+    private Handler characterHandler = new Handler(){
+        public void handleMessage(Message msg){
+            ImageView characterImage = (ImageView) findViewById(R.id.logo);
+            switch (msg.what){
+                case 0:
+                    characterImage.setBackgroundResource(R.drawable.human);
+                    break;
+                case 1:
+                    characterImage.setBackgroundResource(R.drawable.binbo);
+                    break;
+                case 2:
+                    characterImage.setBackgroundResource(R.drawable.nekoze2);
+                    break;
+                case 3:
+                    characterImage.setBackgroundResource(R.drawable.nekoze3);
+                    break;
+                case 4:
+                    characterImage.setBackgroundResource(R.drawable.nail);
+                    break;
+            }
+        }
+    };
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
